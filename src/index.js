d2c077a6c0e6e8da4ff344c4566b89735d4ab333
AndrewMartial <andrewomosh44@gmail.com>
2022-08-26 01:36:29 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let list = document.querySelectorAll('#list');
+  console.log(list);
+
+  let input = document.querySelector('#new-task-description')
+  let taskForm = document.querySelector('#create-task-form');
+
+  console.log(taskForm);
+  taskForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let list = document.querySelector('#tasks')
+      let listContent = document.createElement('li')
+      listContent.innerText = input.value
+      // console.log(listContent)
+      
+      let deleteButton = document.createElement('button')
+      deleteButton.innerText = 'x'
+
+      list.appendChild(listContent);
+      listContent.appendChild(deleteButton);
+      taskForm.reset()
+
+
+      // delete/remove task
+      deleteButton.addEventListener('click', (e) => {
+          listContent.remove();
+
+      })
+
+
+      //resets input form
+      taskForm.reset()
+  })
+
+
+
 });
